96807b9c641fab60c4ee000c2f5907df2efb505d
John Haugabook <johnhaugabook@gmail.com>
2023-07-13 18:33:51 -0400

    roffit: update broken links script
    Debugged script and added short descriptive comments.

--- fixBrokenLinks.sh
+++ fixBrokenLinks.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-_lineNumber=$(grep -n "&quot;\<a " out.dump | cut -d ":" -f 1)
-sed -i -E "${_lineNumber}s|(href=\")([^\"]*)(&quot;)|\1\2|g" out.dump
-_lineNumberhttp=$(grep -n '("\>http://' out.dump | cut -d ":" -f 1)
-sed -i "${_lineNumberhttp}s|)\">http://|\" data-null-http=\"1\">http://|g" out.dump
-_nullhttp=$(grep -n 'data-null-http=\"1\"' out.dump | cut -d ":" -f 1)
-sed -i -E "${_nullhttp}s|href=\"([^\"]*)\" data-null-http=\"1\"|href=\"javascript:void(0)\" data-null-http=\"0\"|g" out.dump
-_lineNumberhttp=$(grep -n 'href="http://"' out.dump | cut -d ":" -f 1)
-sed -i "${_lineNumberhttp}s|href=\"http://\"|href=\"javascript:void(0)\"|g" out.dump
-_lineNumberftp=$(grep -n '("\>ftp://' out.dump | cut -d ":" -f 1)
-sed -i "${_lineNumberftp}s|)\">ftp://|\" data-null-link=\"1\">ftp://|g" out.dump
-_nullftp=$(grep -n 'data-null-link=\"1\"' out.dump | cut -d ":" -f 1)
-sed -i -E "${_nullftp}s|href=\"([^\"]*)\" data-null-link=\"1\"|href=\"javascript:void(0)\" data-null-link=\"0\"|g" out.dump
-_lineNumberftp=$(grep -n 'href="ftp://"' out.dump | cut -d ":" -f 1)
-sed -i "${_lineNumberftp}s|href=\"ftp://\"|href=\"javascript:void(0)\"|g" out.dump
+# This is a supporting script that runs after the html is
+# generated. It fixes links followed by punctuations and
+# changes empty protocols to null links.
 
+# Fix links where quoted.
+_lineNumber=$(grep -n "&quot;\<a " testpage.dump | cut -d ":" -f 1)
+sed -i -E "${_lineNumber}s|(href=\")([^\"]*)(&quot;)|\1\2|g" testpage.dump
+
+# Fix links followed by punctuations.
+sed -i -E "s|(href=\"[^\"]*)([[:punct:]]+)(\">)([^\"]+)&quot;([[:punct:]]*)(</a>)|\1\3\4\6\"\5|g" testpage.dump
+sed -i -E "s|(href=\"[^\"]*)([[:punct:]]+)(\">)([^\"]*)([[:punct:]]*)(</a>)|\1\3\4\6\5|g" testpage.dump
+
+# Fix incomplete http://, https://, and ftp://
+# http
+_lineNumberhttp=$(grep -n 'href="http://"' testpage.dump | cut -d ":" -f 1)
+sed -i -E "${_lineNumberhttp}s|href=\"http://\">(http://)(\))([[:punct:]])(</a>)|href=\"javascript:void(0)\">\1\4\2\3|g" testpage.dump # e.g. (http://),
+sed -i -E "${_lineNumberhttp}s|href=\"http://\)\">(http://)(\))([[:punct:]])(</a>)|href=\"javascript:void(0)\">\1\4\2\3|g" testpage.dump # e.g. (http://) 
+sed -i "s|href=\"http://\"|href=\"javascript:void(0)\"|g" testpage.dump # e.g. http://,
+sed -i "s|<a href=\"http:\">http://</a>|<a href=\"javascript:void(0)\">http://</a>|g" testpage.dump # e.g. ("http://")
+sed -i "s|<a href=\"http:/\">http://</a>|<a href=\"javascript:void(0)\">http://</a>|g" testpage.dump # e.g. "http://",
+# https
+_lineNumberhttp=$(grep -n 'href="https://"' testpage.dump | cut -d ":" -f 1)
+sed -i -E "${_lineNumberhttp}s|href=\"https://\">(https://)(\))(</a>)|href=\"javascript:void(0)\">\1\3\2|g" testpage.dump # e.g. (https://),
+sed -i -E "${_lineNumberhttp}s|href=\"https://\)\">(https://)(\))([[:punct:]])(</a>)|href=\"javascript:void(0)\">\1\4\2\3|g" testpage.dump # e.g. (https://)
+sed -i "s|href=\"https://\"|href=\"javascript:void(0)\"|g" testpage.dump # e.g. https://,
+sed -i "s|<a href=\"https:\">https://</a>|<a href=\"javascript:void(0)\">https://</a>|g" testpage.dump # e.g. ("https://")
+sed -i "s|<a href=\"https:/\">https://</a>|<a href=\"javascript:void(0)\">https://</a>|g" testpage.dump # e.g. "https://",
+# ftp
+_lineNumberftp=$(grep -n 'href="ftp://"' testpage.dump | cut -d ":" -f 1)
+sed -i -E "${_lineNumberhttp}s|href=\"ftp://\">(ftp://)(\))(</a>)|href=\"javascript:void(0)\">\1\3\2|g" testpage.dump # e.g. (ftp://),
+sed -i -E "${_lineNumberhttp}s|href=\"ftp://\)\">(ftp://)(\))([[:punct:]])(</a>)|href=\"javascript:void(0)\">\1\4\2\3|g" testpage.dump # e.g. (ftp://)
+sed -i "s|href=\"ftp://\"|href=\"javascript:void(0)\"|g" testpage.dump # e.g. ftp://,
+sed -i "s|<a href=\"ftp:\">ftp://</a>|<a href=\"javascript:void(0)\">ftp://</a>|g" testpage.dump # e.g. ("ftp://")
+sed -i "s|<a href=\"ftp:/\">ftp://</a>|<a href=\"javascript:void(0)\">ftp://</a>|g" testpage.dump # e.g. ftp://
